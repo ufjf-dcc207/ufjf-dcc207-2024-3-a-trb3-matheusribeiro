49273463c0d54827bd2a60a3484e9b882f69d71b
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2025-03-13 17:13:02 -0300

    adiciona estilização no form e livro

--- src/components/Livro.tsx
+++ src/components/Livro.tsx
@@ -1,15 +1,19 @@
-import './Livro.css'
+import "./Livro.css";
 
-export default function Livro(){
-    return(
-        <div className='livro'>
-            <div className='textoContainer'>
-                <p className='texto'>Nome: Sherlock Holmes</p>
-                <p className='texto'>Autor: Arthur Conan Doyle</p>
-                <p className='texto'>Número de Páginas: 200</p>
-                <p className='texto'>Progresso de Leitura: 20/200</p>
-                <p className='texto'>Status: Lendo</p>
-            </div>
-        </div>
-    )
+export default function Livro() {
+  return (
+    <div className="livro">
+      <div className="textoContainer">
+        <p className="texto">Nome: Sherlock Holmes</p>
+        <p className="texto">Autor: Arthur Conan Doyle</p>
+        <p className="texto">Número de Páginas: 200</p>
+        <p className="texto">Progresso de Leitura: 20/200</p>
+        <p>Página que parou: </p>
+        <input type="number" min={0} />
+        <p className="texto">Status: Lendo</p>
+
+        <button type="submit">Editar</button>
+      </div>
+    </div>
+  );
 }
